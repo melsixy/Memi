f1c16814fbec42a3f03fdcc861049dde981d6893
vik <vik@gmail.com>
2023-01-12 11:33:42 +0200

    zdr

---
+++ chisloOt1DoN/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chisloOt1DoN
+{
+    internal class Program
+    {
+        static void Main(string[] argsa)
+        {
+            //mihaela i mariea 
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i < n; i += 3)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
